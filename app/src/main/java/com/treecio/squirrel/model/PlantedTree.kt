5846d7d19825b5b27ad9a77cdbfc3d9cd8cc3b12
padr31 <padr31@users.noreply.github.com>
2018-03-10 19:44:56 +0000

    Treetype

--- app/src/main/java/com/treecio/squirrel/model/PlantedTree.kt
+++ app/src/main/java/com/treecio/squirrel/model/PlantedTree.kt
@@ -6,8 +6,11 @@ class PlantedTree(
         val id: String = UUID.randomUUID().toString(),
         val name: String,
         val story: String,
+        val treetype: Int,
 
         val time: Long,
         val lat: Double,
         val lon: Double
+
 )
+
