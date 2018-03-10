666176cd30ff362e9f21438e8e58fa134fa2688e
Juraj Mičko <micko.juraj64@gmail.com>
2018-03-10 13:37:52 +0000

    Replace Robospice library with OkHttp

--- app/src/main/java/com/treecio/squirrel/network/model/abs/JsonModel.kt
+++
@@ -1,6 +0,0 @@
-package com.treecio.squirrel.network.model.abs
-
-/**
- * Abstraction coupling individual implementations of json models.
- */
-abstract class JsonModel : JsonObject
--- app/src/main/java/com/treecio/squirrel/network/response/EmptyResponse.kt
+++
@@ -1,5 +0,0 @@
-package com.treecio.squirrel.network.response
-
-import com.treecio.squirrel.network.model.abs.JsonObject
-
-class EmptyResponse : JsonObject
