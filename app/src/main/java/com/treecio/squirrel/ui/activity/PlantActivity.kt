0d6010b045e688fa776d1c1c0a76e3c606fbfddd
Juraj Mičko <micko.juraj64@gmail.com>
2018-03-10 16:46:04 +0000

    Make PlantActivity a child activity

--- app/src/main/java/com/treecio/squirrel/ui/activity/PlantActivity.kt
+++ app/src/main/java/com/treecio/squirrel/ui/activity/PlantActivity.kt
@@ -1,9 +1,9 @@
 package com.treecio.squirrel.ui.activity
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v4.app.FragmentManager
 import android.support.v4.app.FragmentTransaction
+import android.support.v7.app.AppCompatActivity
 import com.treecio.squirrel.R
 import com.treecio.squirrel.ui.fragment.PlantFragment
 
@@ -15,6 +15,8 @@ class PlantActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_plant)
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
         val mFragmentManager: FragmentManager = supportFragmentManager
         val mFragmentTransaction: FragmentTransaction = mFragmentManager.beginTransaction()
