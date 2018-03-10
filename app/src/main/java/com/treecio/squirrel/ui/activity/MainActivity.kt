5ff46ccd7a97598a1aac4b908585b1b2eb09a213
Juraj Mičko <micko.juraj64@gmail.com>
2018-03-10 11:58:26 +0000

    MainFragment

--- app/src/main/java/com/treecio/squirrel/ui/activity/MainActivity.kt
+++ app/src/main/java/com/treecio/squirrel/ui/activity/MainActivity.kt
@@ -1,13 +1,27 @@
 package com.treecio.squirrel.ui.activity
 
 import android.os.Bundle
-import android.support.v7.app.AppCompatActivity
 import com.treecio.squirrel.R
+import com.treecio.squirrel.ui.fragment.MainFragment
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : BaseActivity() {
+
+    companion object {
+        const val MAP_FRAGMENT_TAG = "fragment_map"
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        init(savedInstanceState)
+    }
+
+    override fun initNew() {
+        val fragment = MainFragment()
+        supportFragmentManager
+                .beginTransaction()
+                .replace(R.id.content, fragment, MAP_FRAGMENT_TAG)
+                .commit()
     }
 }
