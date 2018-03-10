3a92cd9354ad069351386f6e3ad72d5b14df59f9
padr31 <padr31@users.noreply.github.com>
2018-03-10 13:40:24 +0000

    Add PlantActivity

--- app/src/main/java/com/treecio/squirrel/ui/fragment/PlantFragment.kt
+++ app/src/main/java/com/treecio/squirrel/ui/fragment/PlantFragment.kt
@@ -1,4 +1,4 @@
-package com.treecio.squirrel.squirrel
+package com.treecio.squirrel.ui.fragment
 
 import android.content.Context
 import android.net.Uri
@@ -7,6 +7,7 @@ import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.treecio.squirrel.R
 
 class PlantFragment : Fragment() {
 
