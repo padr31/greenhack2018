6f3bb0d44aa8ea19ccd2c9ad4b801bd36b17fcca
Juraj Mičko <micko.juraj64@gmail.com>
2018-03-10 11:11:48 +0000

    PlantedTree

---
+++ app/src/main/java/com/treecio/squirrel/squirrel/model/PlantedTree.kt
@@ -0,0 +1,14 @@
+package com.treecio.squirrel.squirrel.model
+
+class PlantedTree (
+        val name: String,
+        val story: String,
+
+        val time: Long,
+        val lat: Double,
+        val lon: Double
+) {
+
+
+
+}
